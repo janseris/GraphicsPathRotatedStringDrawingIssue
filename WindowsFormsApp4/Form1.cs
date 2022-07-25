0c217fd62c7c4d2c8e2e823207a5cac409527c34
Jan Seriš <jan.seris@seznam.cz>
2022-07-25 13:29:40 +0200

    all symbols rendering correctly

--- WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/Form1.cs
@@ -27,16 +27,91 @@ namespace WindowsFormsApp4
             this.MouseClick += (s, e) => { if (e.Button == MouseButtons.Left) { if (Timer.Enabled) { Timer.Stop(); } else { Timer.Start(); } } if (e.Button == MouseButtons.Right) { Rotation = 0; Redraw(); } };
         }
 
+        //private void Form1_Paint(object sender, PaintEventArgs e)
+        //{
+        //    var textDefault = GetTextPath(e.Graphics, StringFormat.GenericDefault);
+        //    var textVerticalCenter = GetTextPath(e.Graphics, new StringFormat() { LineAlignment = StringAlignment.Center}); //as provided by reza-aghaei
+
+        //    DrawTextPath(e.Graphics, textDefault); textDefault.Dispose();
+        //    DrawTextPath(e.Graphics, textVerticalCenter); textVerticalCenter.Dispose();
+
+        //    e.Graphics.DrawEllipse(new Pen(Color.Black, width: 5), new RectangleF(Position, new Size(1, 1)));
+        //}
+
+
+        //https://docs.microsoft.com/en-us/windows/apps/design/style/segoe-ui-symbol-font
+        string arrowTopLeftSymbol => ((char)0xE742).ToString(); //points to top left corner
+        string arrowTopRightSymbol => ((char)0xE8AD).ToString(); //points to top right corner
+        string pinSymbol => ((char)0xE840).ToString(); //points to bottom left corner
+        string arrowBottomRightSymbol => ((char)0xE741).ToString(); //points to bottom right corner
+
+
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            var textDefault = GetTextPath(e.Graphics, StringFormat.GenericDefault);
-            var textVerticalCenter = GetTextPath(e.Graphics, new StringFormat() { LineAlignment = StringAlignment.Center}); //as provided by reza-aghaei
-             
-            DrawTextPath(e.Graphics, textDefault); textDefault.Dispose();
-            DrawTextPath(e.Graphics, textVerticalCenter); textVerticalCenter.Dispose();
+            DrawText(e.Graphics, pinSymbol); //draws OK
+            DrawText(e.Graphics, arrowTopLeftSymbol); //draws OK
+            
+            DrawText(e.Graphics, arrowTopRightSymbol);
+            DrawText(e.Graphics, arrowBottomRightSymbol);
 
             e.Graphics.DrawEllipse(new Pen(Color.Black, width: 5), new RectangleF(Position, new Size(1, 1)));
+        }
 
+        private void DrawText(Graphics g, string text, bool drawBoundingRectangle = false)
+        {
+            using (var path = new GraphicsPath())
+            {
+                path.AddString(text, new FontFamily("Segoe MDL2 Assets"), (int)FontStyle.Regular, 120, new PointF(0, 0), StringFormat.GenericDefault);
+
+                var br = path.GetBounds(); //"bounding rectangle"
+                var bp = br.Location; //"base point"?
+
+                SizeF topLeftTranslate = new SizeF(0, 0); //OK
+                SizeF bottomLeftTranslate = new SizeF(0, -br.Height); //OK
+                SizeF topRightTranslate = new SizeF(-br.Width, 0); 
+                SizeF bottomRightTranslate = new SizeF(-br.Width, -br.Height); 
+
+                var m1 = new Matrix();
+                m1.Translate(Position.X - bp.X, Position.Y - bp.Y); //set draw to position with correction by unwanted string drawing offset ("bp")
+                SizeF relativeTranslate = SizeF.Empty; //default
+                //OK
+                if(text == arrowTopLeftSymbol)
+                {
+                    relativeTranslate = topLeftTranslate; //in fact, same as default
+                } 
+                //OK
+                if(text == pinSymbol)
+                {
+                    relativeTranslate = bottomLeftTranslate;
+                }
+                //???
+                if(text == arrowTopRightSymbol)
+                {
+                    relativeTranslate = topRightTranslate;
+                }
+                if(text == arrowBottomRightSymbol)
+                {
+                    relativeTranslate = bottomRightTranslate;
+                }
+                m1.Translate(relativeTranslate.Width, relativeTranslate.Height); //switch to different drawing position
+                path.Transform(m1);
+
+                // To rotate arround the center of the path:
+                //PointF o = new PointF( br.Width / 2, br.Height / 2 );
+                //var m2 = new Matrix( );
+                //m2.Translate( -o.X, -o.Y );
+                //path.Transform( m2 );
+
+                var m = new Matrix();
+                m.RotateAt(Rotation, Position);
+                path.Transform(m);
+
+                g.DrawPath(new Pen(Brushes.Black), path);
+                if (drawBoundingRectangle)
+                {
+                    g.DrawRectangles(new Pen(Brushes.Black), new RectangleF[] { path.GetBounds() });
+                }
+            }
         }
 
         private void DrawRectangle(Graphics g, GraphicsPath originalPath, bool verticalCenter = false)
